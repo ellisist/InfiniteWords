ea8a7d9f97865d6916b07e7505ebf362be3cf914
Matthew Ellis <matt.ellis012@gmail.com>
2012-01-14 16:22:43 -0500

    Cleared for CAPSTONE

--- letterbag.py
+++
@@ -1,9 +0,0 @@
-class Letterbag:
-    """provides a way to get next 'random' letter tile"""
-    def __init__(self):
-        self.tiles = []
-        # TODO: populate tiles with appropriate distribution of tiles
-
-    def get_letter(self):
-        # this can be rewritten to do whatever.
-        return self.tiles.pop()
